e53c14f158c24e2f85686afea146ca4394adf2c3
Swasti Ranjan Senapati <swasti_151112@gita.edu.in>
2018-07-13 21:06:13 +0530

    Add files via upload

---
+++ app/src/main/java/com/examples/admin/transporttracker/TrackerService.java
@@ -0,0 +1,81 @@
+package com.examples.admin.transporttracker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.example.transporttracker.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.Manifest;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+public class TrackerService extends Service {
+
+    private static final String TAG = TrackerService.class.getSimpleName();
+
+    @Override
+    public IBinder onBind(Intent intent) {return null;}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildNotification();
+        loginToFirebase();
+    }
+
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+        // Create the persistent notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.notification_text))
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.ic_tracker);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "received stop broadcast");
+            // Stop the service when the notification is tapped
+            unregisterReceiver(stopReceiver);
+            stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+        // Functionality coming next step
+    }
+
+    private void requestLocationUpdates() {
+        // Functionality coming next step
+    }
+
+}
